e23303a4341fdb6cecc78b8542e1bf233a1c42c8
Carlos Monteiro <offbyte@gmail.com>
2018-03-11 21:00:37 +0000

    First working concept
    - AsyncTaskLoader
    - NetworkUtils
    - Getting data with api key
    - Showing Raw Json

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -1,12 +1,27 @@
 package com.example.android.popularmovies;
 
+import android.annotation.SuppressLint;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.example.android.popularmovies.utils.NetworkUtils;
 import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity {
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
+
+    private TextView mResultView;
+
+    private static final int MOVIE_LOADER_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +30,55 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView picassoIv = findViewById(R.id.picasso_iv);
         Picasso.with(MainActivity.this).load("http://i.imgur.com/DvpvklR.png").into(picassoIv);
+        mResultView = findViewById(R.id.results_tv);
 
-        //test
+        getSupportLoaderManager().initLoader(MOVIE_LOADER_ID, null, this);
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    @Override
+    public Loader<String> onCreateLoader(int id, final Bundle args) {
+        return new AsyncTaskLoader<String>(this) {
+
+            String mMovieResults;
+
+            @Override
+            protected void onStartLoading() {
+
+                if (mMovieResults != null) {
+                    deliverResult(mMovieResults);
+                } else {
+                    forceLoad();
+                }
+            }
+
+            @Override
+            public String loadInBackground() {
+
+                URL moviesRequestUrl = NetworkUtils.buildUrl(MainActivity.this);
+                try {
+                    String jsonResponse = NetworkUtils.getJsonResponseFromUrl(moviesRequestUrl);
+                    return jsonResponse;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, String data) {
+        if (data != null) {
+            mResultView.setText(data);
+        } else {
+            mResultView.setText("error");
+        }
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
+
     }
 }
---
+++ app/src/main/java/com/example/android/popularmovies/utils/NetworkUtils.java
@@ -0,0 +1,57 @@
+package com.example.android.popularmovies.utils;
+
+import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+
+import com.example.android.popularmovies.R;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+public class NetworkUtils {
+
+    public static final String TAG = NetworkUtils.class.getSimpleName();
+
+    private static final String THE_MOVIE_DB_BASE_URL = "http://api.themoviedb.org/3/movie/";
+    private static final String PARAM_API_KEY = "api_key";
+
+
+    public static URL buildUrl(Context context) {
+
+        Uri buildUri = Uri.parse(THE_MOVIE_DB_BASE_URL + "popular").buildUpon()
+                .appendQueryParameter(PARAM_API_KEY, context.getString(R.string.tmdb_api_key))
+                .build();
+
+        URL url = null;
+        try {
+            url = new URL(buildUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+    public static String getJsonResponseFromUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
